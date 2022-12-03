5b06acca023167eb4896c66ac0bf4fffa26e9a40
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-12-03 17:20:20 +0100

    feat: implement home page template using firebase collection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,42 @@
-import BooksList from "../components/BooksList";
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import { collection, getDocs } from "firebase/firestore";
+import SingleBook from "../components/SingleBook";
+
+interface Book {
+  id: string;
+  title: string;
+  author: string;
+  read: boolean;
+  genre: string;
+}
 
 function Home() {
+  const [books, setBooks] = useState<Array<Book> | null>(null);
+
+  useEffect(() => {
+    const ref = collection(db, "books");
+    getDocs(ref).then((snapshot) => {
+      let results: any[] = [];
+      snapshot.docs.forEach((doc) => {
+        results.push({ id: doc.id, ...doc.data() });
+      });
+      setBooks(results);
+    });
+  }, []);
+
   return (
     <div>
       <button className="bg-sky-600 text-gray-100 px-4 py-2 rounded">
         add book
       </button>
-      <BooksList />
+      {books ? (
+        books.map((book, index) => <SingleBook book={book} key={index} />)
+      ) : (
+        <div>
+          <h2>loading...</h2>
+        </div>
+      )}
     </div>
   );
 }
